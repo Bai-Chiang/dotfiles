330067e34916b7f3400e8e140cdf62dd15519842
Bai-Qiang Qiang <qiangbq@gmail.com>
2021-03-29 07:42:31 -0400

    add .screenlayout

---
+++ .screenlayout/docked.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output DP-3 --mode 1920x1080 --pos 5760x0 --rotate right  --output HDMI-0 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
