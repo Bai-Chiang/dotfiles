7682fa1bacbe200faf8023e6d4ac1b8f92e42a87
Bai-Qiang Qiang <qiangbq@gmail.com>
2021-05-08 17:53:49 -0400

    update i3 config

--- .screenlayout/presentation.sh
+++ .screenlayout/presentation.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --mode 3840x2160 --pos 0x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 0x2160 --rotate normal
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 0x2160 --rotate normal
