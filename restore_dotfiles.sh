4295302a37fe47002b9edeabfa17380500063529
Bai-Chiang <BaiChiang.Chiang@gmail.com>
2021-11-26 15:03:02 -0500

    Update restore_dotfiles.sh

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-git clone --bare <dotfiles-repo-url> $HOME/.dotfiles
+git clone --bare git@github.com:Bai-Chiang/dotfiles.git $HOME/.dotfiles
 
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
