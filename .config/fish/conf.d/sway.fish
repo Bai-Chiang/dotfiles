8b1c750fd2f89fd28d249b90a920a27b2d53c304
Bai-Chiang <BaiChiang.Chiang@gmail.com>
2021-11-27 20:00:57 -0500

    update

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -2,8 +2,11 @@
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   # let kde application like dolphin show correct theme
-  set -x DESKTOP_SESSION kde
-  set -x GDK_BACKEND wayland
-  exec sway
+  #set -x DESKTOP_SESSION kde
+  #set -x GDK_BACKEND wayland
+  set -x GTK_THEME Adwaita:dark
+  set -x QT_QPA_PLATFORMTHEME kde
+  set -x WLR_NO_HARDWARE_CURSORS 1
+  exec sway --my-next-gpu-wont-be-nvidia
 end
 
