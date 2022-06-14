7417e939e69a3335ef3c36cbe80aeb85706f4b2a
Bai-Chiang <BaiChiang.Chiang@gmail.com>
2022-06-14 10:24:51 -0400

    add .gitignore

--- .config/fish/conf.d/kde.fish
+++ .config/fish/conf.d/kde.fish
@@ -1,6 +1,6 @@
-# If running from tty2 start sway
-set TTY2 (tty)
-if test -z "$DISPLAY"; and test $TTY2 = "/dev/tty2"
-    XDG_SESSION_TYPE=wayland startplasma-wayland
-end
+# If running from tty2 start KDE
+#set TTY2 (tty)
+#if test -z "$DISPLAY"; and test $TTY2 = "/dev/tty2"
+#    XDG_SESSION_TYPE=wayland startplasma-wayland
+#end
 
