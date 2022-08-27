f1964c78ef93163b913c67c9fdc4887ed5f019f7
Bai-Chiang <contact@bai-chiang.com>
2022-08-27 01:17:33 +0000

    update

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -3,8 +3,7 @@ set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   set -x GTK_THEME Adwaita:dark
   set -x QT_QPA_PLATFORMTHEME kde
-  # set -x WLR_NO_HARDWARE_CURSORS 1   # for nvidia driver
   set -x XDG_CURRENT_DESKTOP sway
-  exec sway # --unsupported-gpu
+  exec sway
 end
 
