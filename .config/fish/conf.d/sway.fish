963dddaefd35b68b3bdd3b3fc7b231ca31fbe87e
Bai-Chiang <contact@bai-chiang.com>
2025-11-03 08:30:50 -0600

    Update configs

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -5,7 +5,10 @@ if status is-login
         set -x GTK_THEME Adwaita:dark
         set -x QT_QPA_PLATFORMTHEME kde
         set -x QT_QPA_PLATFORM wayland
+        set -x XDG_SESSION_TYPE wayland
         set -x XDG_CURRENT_DESKTOP sway
+        set -x XDG_SESSION_DESKTOP sway
+        set -x LIBSEAT_BACKEND logind
         exec sway
     end
 end
